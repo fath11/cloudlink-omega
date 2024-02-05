c082202737554313cb461c8757b9fa91bfd26a68
fath11 <124476952+fath11@users.noreply.github.com>
2024-02-05 11:16:00 +0800

    update gandi ver

--- client/cloudlinkomega_gandi.js
+++ client/cloudlinkomega_gandi.js
@@ -11,7 +11,6 @@
 
     class CloudLinkOmega {
         constructor(runtime, virtualMachine) {
-            this.runtime = runtime; // Runtime
             if (this.runtime._events['QUESTION'] instanceof Array) {
                 for (const value of this.runtime._events['QUESTION']) {
                     const v = hijack(value)
@@ -26,6 +25,7 @@
             if (!virtualMachine)
             throw new Error('lpp cannot get Virtual Machine instance.')
             this.vm = virtualMachine //VM
+            this.runtime = virtualMachine.runtime;
             this.targets = runtime.targets; // Access variables
             this.uuid = null;
             this.username = null;
